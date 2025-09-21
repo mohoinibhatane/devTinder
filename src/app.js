d700cce5e45d3cae229823d92c8dc4b3dd00cd5d
PadmakarKendre <kendrepadmakar15@gmail.com>
2025-09-21 15:22:05 +0530

    get user by email

--- src/app.js
+++ src/app.js
@@ -5,6 +5,26 @@ const { User } = require("./models/user.js");
 
 app.use(express.json());
 
+//get user by email
+app.get("/users", async(req, res) => {
+    const userEmail = req.body.email;
+
+    try{
+      console.log(userEmail);
+
+      const user = await User.find({ email: userEmail }); //{ fieldNameInDB: valueToMatch }
+
+      if (!user) {
+        res.send("user not found");
+      } else {
+        res.send(user);
+      }
+    }
+    catch(error){
+        res.status(400).send("something went wrong");
+    }
+})
+
 app.post("/signup", async (req, res) => {
   //creating instance of user
 
